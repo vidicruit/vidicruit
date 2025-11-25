f7a4f5aa1715d443d28edaaefd169c80b6615cc2
vidicruit <cipriandumitrascu88@gmail.com>
2025-11-25 17:11:32 +0200

    disable eslint during builds

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    // nu mai opri build-ul dacă ESLint găsește probleme
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
